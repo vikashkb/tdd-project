07b76ca1e691ae86281c748f574ab4cc4f0f68ee
vikashkb <vikash.kumar.banjare@intel.com>
2022-12-27 01:26:38 -0500

    test: verified behavior when modifying an existing exchange rate

--- py/test_money.py
+++ py/test_money.py
@@ -68,11 +68,12 @@ class TestMoney(unittest.TestCase):
                 "Missing exchange rate\(s\):\[USD\->Kalganid,EUR->Kalganid,KRW->Kalganid]",
                 ):portfolio.evaluate(self.bank, "Kalganid")
 
-    def testConversion(self):
-        bank = Bank()
-        bank.addExchangeRate("EUR", "USD", 1.2)
+    def testConversionWithDifferentRatesBetweenTwoCurrencies(self):
         tenEuros = Money(10, "EUR")
-        self.assertEqual(bank.convert(tenEuros, "USD"), Money(12, "USD"))
+        self.assertEqual(self.bank.convert(tenEuros, "USD"), Money(12, "USD"))
+
+        self.bank.addExchangeRate("EUR", "USD", 1.3)
+        self.assertEqual(self.bank.convert(tenEuros, "USD"), Money(13, "USD"))
 
     def testConversionWithMissingExchangeRate(self):
         bank = Bank()
